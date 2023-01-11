45974c4cd9595574a1cd0b03463243f21c9bfd13
thedarkarchitect <romanreynolds240@gmail.com>
2023-01-11 13:02:17 +0300

    third commit

--- dictionary/engdict/views.py
+++ dictionary/engdict/views.py
@@ -5,4 +5,5 @@ def index(request):
     return render(request, "index.html")
 
 def word(request):
+    search = request.GET.get('search')#this gets the word put in the search bar using the name of the input the search bar
     return render(request, "word.html" )
