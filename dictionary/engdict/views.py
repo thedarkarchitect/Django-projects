7d610c500d06c0cb41642ea2bde4056f486efc1f
thedarkarchitect <romanreynolds240@gmail.com>
2023-01-11 12:57:58 +0300

    second commit

--- dictionary/engdict/views.py
+++ dictionary/engdict/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+def word(request):
+    return render(request, "word.html" )
