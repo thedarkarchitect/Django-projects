f53e033f0c72a02a0e79ab32190f8c6f7cf87e60
thedarkarchitect <romanreynolds240@gmail.com>
2023-01-11 14:04:29 +0300

    final commit

--- dictionary/engdict/views.py
+++ dictionary/engdict/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from pydictionary import Dictionary
 
 # Create your views here.
 def index(request):
@@ -6,4 +7,13 @@ def index(request):
 
 def word(request):
     search = request.GET.get('search')#this gets the word put in the search bar using the name of the input the search bar
-    return render(request, "word.html" )
+    dictionary = Dictionary()
+    meaning = dictionary.meaning(search)
+    synonyms = dictionary.synonym(search)
+    antonyms = dictionary.antonym(search)
+
+    return render(request, "word.html" , {
+        'meaning': meaning['Noun'][0],
+        'synonyms': synonyms,
+        'antonyms': antonyms
+    })
