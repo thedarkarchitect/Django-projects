c9d0fd31d68ec86c74ffd8e341e45b012d0962ef
thedarkarchitect <romanreynolds240@gmail.com>
2023-01-13 11:52:05 +0300

    fourth commit

--- todolist3/base/views.py
+++ todolist3/base/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import Task
 
 # Create your views here.
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+
+
+class TaskDetail(DetailView):
+    model = Task
+
+    context_object_name = 'task' #customize the reference object name in htmls
