598ebf4b23e5e305826fc13d30cf7e0bf3a1edec
thedarkarchitect <romanreynolds240@gmail.com>
2023-01-14 15:14:19 +0300

    tenth commit

--- todolist3/base/urls.py
+++ todolist3/base/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views  import TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView
+from .views  import TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView, CustomLoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),#this will be path for the login page
+    path('logout/', CustomLoginView.as_view(next_page='login'), name='logout'),#this takes the user to the login view after logout
     path('', TaskList.as_view(), name='tasks'),#this is our home page
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),#this uses the primary key of items to show details or tap into the tasks
     path('task-create/', TaskCreate.as_view(), name='task-create'),#This  view allows to create a task that is sent to the tasks page
--- todolist3/base/views.py
+++ todolist3/base/views.py
@@ -5,7 +5,17 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Task
 from django.urls import reverse_lazy #helps redirect too aspecific page after submission of a 
 
+from django.contrib.auth.views import LoginView
+
 # Create your views here.
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+    
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+
 class TaskList(ListView):#this view displays the database contents
     model = Task
     context_object_name = 'tasks'#this allows us to tap into the database using the context name as a variable('tasks')
@@ -31,3 +41,4 @@ class DeleteView(DeleteView):
     model = Task
     context_object_name = 'task'
     success_url = reverse_lazy('tasks')
+
