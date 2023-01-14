777cfbc9ba897ba12e6cbcc69c4d455fdba5b0fc
thedarkarchitect <romanreynolds240@gmail.com>
2023-01-14 14:44:45 +0300

    ninth commit

--- todolist3/base/urls.py
+++ todolist3/base/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views  import TaskList, TaskDetail, TaskCreate, TaskUpdate
+from .views  import TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),#this is our home page
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),#this uses the primary key of items to show details or tap into the tasks
     path('task-create/', TaskCreate.as_view(), name='task-create'),#This  view allows to create a task that is sent to the tasks page
-    path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update')
+    path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
+    path('task-delete/<int:pk>/', DeleteView.as_view(), name='task-delete')
 ]
