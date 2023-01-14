85f63f492081efeb7cf2cddb5cba8654ba5a59bd
thedarkarchitect <romanreynolds240@gmail.com>
2023-01-14 13:35:36 +0300

    sixth commit

--- todolist3/base/urls.py
+++ todolist3/base/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views  import TaskList, TaskDetail
+from .views  import TaskList, TaskDetail, TaskCreate
 
 urlpatterns = [
-    path('', TaskList.as_view(), name='tasks'),
-    path('task/<int:pk>/', TaskDetail.as_view(), name='task')
+    path('', TaskList.as_view(), name='tasks'),#this is our home page
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),#this uses the primary key of items to show details or tap into the tasks
+    path('task-create', TaskCreate.as_view(), name='task-create')#This  view allows to create a task that is sent to the tasks page
 ]
--- todolist3/base/views.py
+++ todolist3/base/views.py
@@ -3,20 +3,21 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
 from .models import Task
+from django.urls import reverse_lazy #helps redirect too aspecific page after submission of a 
 
 # Create your views here.
-class TaskList(ListView):
+class TaskList(ListView):#this view displays the database contents
     model = Task
-    context_object_name = 'tasks'
+    context_object_name = 'tasks'#this allows us to tap into the database using the context name as a variable('tasks')
 
 
-class TaskDetail(DetailView):
+class TaskDetail(DetailView):#this view shows the database contents in detail
     model = Task
-
     context_object_name = 'task' #customize the reference object name in htmls
     #customize the template name
     #template_name = 'base/task.html'
 
-class TaskCreate(CreateView):
+class TaskCreate(CreateView):#this views allows user to create an item then save it to the database and redirect ot the Tasklist view
     model = Task
-    field = '__all__'
+    fields = '__all__'
+    success_url = reverse_lazy('tasks') #redirects user to the tasks page after creation of task
