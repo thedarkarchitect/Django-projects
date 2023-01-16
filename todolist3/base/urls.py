8d66d8fbd80f3cc6bf62940d67cdf9f125b812ad
thedarkarchitect <romanreynolds240@gmail.com>
2023-01-16 14:32:42 +0300

    twelfth commit

--- todolist3/base/urls.py
+++ todolist3/base/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views  import TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView, LoginViewPage
+from .views  import TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView, LoginViewPage, RegisterForm
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', LoginViewPage.as_view(), name='login'),#this will be path for the login page
     path('logout/', LoginViewPage.as_view(next_page='login'), name='logout'),#this takes the user to the login view after logout
+    path('register/', RegisterForm.as_view(), name='register'),
+    
     path('', TaskList.as_view(), name='tasks'),#this is our home page, where all tasks are viewed
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),#this uses the primary key of items to show details or tap into the tasks
     path('task-create/', TaskCreate.as_view(), name='task-create'),#This  view allows to create a task that is sent to the tasks page
--- todolist3/base/views.py
+++ todolist3/base/views.py
@@ -1,26 +1,41 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from .models import Task
 from django.urls import reverse_lazy #helps redirect too aspecific page after submission of a 
 
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin#this retricts the user that enter the page
-
+from django.contrib.auth.forms import UserCreationForm #Creates a user
+from django.contrib.auth import login#signs in user directly after registering
 # Create your views here.
 class LoginViewPage(LoginView):
-    template_name = 'base/login.html'
-    fields = '__all__'
-    redirect_authenticated_user = True
+    template_name = 'base/login.html'#this locates the page that displays the login form 
+    fields = '__all__'#this shows all the necessary fields for  the login 
+    redirect_authenticated_user = True#this makes sure only registered user are let into the task form
     
     def get_success_url(self):
-        return reverse_lazy('tasks')
+        return reverse_lazy('tasks')#this is a method that redirects the registered user to the tasks page url
+
+class RegisterForm(FormView):
+    template_name = 'base/register'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+
+    
 
 class TaskList(LoginRequiredMixin, ListView):#this view displays the database contents
     model = Task#This is the database
     context_object_name = 'tasks'#this allows us to tap into the database using the context name as a variable('tasks')
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)#this adds tasks to the loggedin user and shows the tasks of only the user logged in
+        context['count'] = context['tasks'].filter(complete=False).count()#know how many tasks are not complete
+
+        return context
 
 class TaskDetail(LoginRequiredMixin, DetailView):#this view shows the database contents in detail
     model = Task
@@ -30,12 +45,16 @@ class TaskDetail(LoginRequiredMixin, DetailView):#this view shows the database c
 
 class TaskCreate(LoginRequiredMixin, CreateView):#this views allows user to create an item then save it to the database and redirect ot the Tasklist view
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks') #redirects user to the tasks page after creation of task
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user #confirms the user logged in and allows requests from the form of that user
+        return super(TaskCreate, self).form_valid(form)#this allows us to continue with whatever the function class does
 
 class TaskUpdate(LoginRequiredMixin, UpdateView):#similar to the create view but all it does return the model prefilled for edittin
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
 
 class DeleteView(LoginRequiredMixin, DeleteView):
