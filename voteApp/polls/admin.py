ad80db20066352ad9a337b7658295b22a54df3e6
thedarkarchitect <romanreynolds240@gmail.com>
2023-01-19 08:49:41 +0300

    third commit

--- voteApp/polls/admin.py
+++ voteApp/polls/admin.py
@@ -2,4 +2,20 @@ from django.contrib import admin
 from .models import Questions, Choices
 
 # Register your models here.
-admin.site.register(Questions)
+
+admin.site.site_header = "VoteApp Amin"
+admin.site.site_title = "VoteApp Admin Area"
+admin.site.index_title = "Welcome to the VoteApp admin area"
+
+class ChoiceInline(admin.TabularInline):
+    model = Choices
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields' : ['question_text']}),
+                    ('Date Information', {'fields': ['pub_date'], 'classes':['collapse']})]
+    inlines = [ChoiceInline]
+
+# admin.site.register(Questions)
+# admin.site.register(Choice)
+admin.site.register(Questions, QuestionAdmin)
--- voteApp/polls/views.py
+++ voteApp/polls/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-from django.http import JsonResponse
+from django.http import JsonResponse, Http404
 
 from .models import Questions, Choices
 
@@ -12,10 +12,34 @@ def index(request):#Gets questions and displayes them
     context = {'latest_question_list': lastest_question_list}
     return render(request, 'polls/index.html', context)
 
-#show specific questions and choices
-# def detail(request, question_id):
-#     try:
-#         question = Questions.objects.get(pk=question_id)#this get the questions for the db by their id 
-#     except Questions.DoesNotExist:
-#         raise Http404("Question does not exit")
-#     return render(request, 'polls/detail.html', {'question':question})
+# show specific questions and choices
+def detail(request, question_id):
+    try:
+        question = Questions.objects.get(pk=question_id)#this get the questions for the db by their id 
+    except Questions.DoesNotExist:
+        raise Http404("Question does not exit")
+    return render(request, 'polls/detail.html', {'question':question})
+
+def results(request, question_id):#Gets questions and displayes the results
+    question = get_object_or_404(Questions, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+def vote(request, question_id):#vote for questino choices available
+    #print(request.POST['choice'])--> this was ued to show the choices in the terminal that we are going to display on the page
+    question = get_object_or_404(Questions, pk=question_id)#this method will get from the question model by pk but if the pk doesn't exist the method will return a 404
+
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choices.DoesNotExist):
+        #redisplay the question voting form
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message':"You didn't selete a choice"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        #after successfull dealin always return an HttpRespONSEredirect
+        #with Post data. this prevents data from being posted twice if a user hit the back button
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id)))
+
