d9eff7733cb26f5ee7ac7677dc373479db5349ba
thedarkarchitect <romanreynolds240@gmail.com>
2023-01-31 22:12:53 +0300

    sixth commit

--- customerMR/accounts/models.py
+++ customerMR/accounts/models.py
@@ -14,6 +14,14 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
 
+
+class Tag(models.Model):#this just shows the tag a product can be
+    name = models.CharField(max_length=200, null=True)
+    
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     CATEGORY = (
         ('Indoor', 'Indoor'),
@@ -25,6 +33,10 @@ class Product(models.Model):
     category = models.CharField(max_length=200, null=True, choices=CATEGORY)
     description = models.CharField(max_length=255, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    tags = models.ManyToManyField(Tag)#different products can have more than one tag so we use a many to many relationship
+
+    def __str__(self):
+        return self.name
 
 class Order(models.Model):
     STATUS = (
@@ -32,7 +44,8 @@ class Order(models.Model):
         ('Out for delivery', 'Out for Delivery'),
         ('Delivered', 'Delivered'),
     )
-    # cutomer =
-    # product = 
+    #foreign keys are justs parents of the child model being created 
+    cutomer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)#when order is deleted the customer will stay in the database but with a null value for order
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
