c57f913445bb3d5f558df96d7c27f13d6dc0bf0f
thedarkarchitect <romanreynolds240@gmail.com>
2023-01-31 16:07:17 +0300

    third commit

--- customerMR/accounts/urls.py
+++ customerMR/accounts/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('', views.index),
+    path('products/', views.products),
+    path('customer/', views.customer)
 ]
--- customerMR/accounts/views.py
+++ customerMR/accounts/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, "")
+    return render(request, 'accounts/dashboard.html')
+
+def products(request):
+    return render(request, 'accounts/products.html')
+
+def customer(request):
+    return render(request, 'accounts/customer.html')
