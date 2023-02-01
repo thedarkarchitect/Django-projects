9638249fe27adccd34df312a2cd7cf96c43ab8d9
thedarkarchitect <romanreynolds240@gmail.com>
2023-02-01 12:39:43 +0300

    seventh commit

--- customerMR/accounts/urls.py
+++ customerMR/accounts/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('products/', views.products),
-    path('customer/', views.customer)
+    path('', views.index, name='index'),
+    path('products/', views.products, name='products'),
+    path('customer/<str:pk>/', views.customer, name='customer'),
+    path('create/', views.createOrder, name='createOrder')
 ]
--- customerMR/accounts/views.py
+++ customerMR/accounts/views.py
@@ -1,12 +1,45 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def index(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    total_customers = customers.count()
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+
+    context = {
+        'orders': orders,
+        'customers':customers,
+        'total_orders' : total_orders,
+        'delivered': delivered,
+        'pending': pending
+    }
+    return render(request, 'accounts/dashboard.html', context)
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Product.objects.all()
 
-def customer(request):
-    return render(request, 'accounts/customer.html')
+    return render(request, 'accounts/products.html', {
+        'products': products
+    })
+
+def customer(request, pk):
+    customer = Customer.objects.get(id=pk)
+    orders = customer.order_set.all()#this will show the orders of the customer with the id
+
+    context = {
+        'customer': customer,
+        'orders': orders
+    }
+    return render(request, 'accounts/customer.html', context)
+
+def createOrder(request):
+    context = {}
+
+    return render(request, 'accounts/order_form.html', context)
+    
