a20a349e8d40ba542200f8d0aa1748ba31da9565
thedarkarchitect <romanreynolds240@gmail.com>
2023-02-09 15:06:22 +0300

    final commit

--- market2/converstation/urls.py
+++ market2/converstation/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from . import views
 
-app_name = 'conversation'
+app_name = 'converstation'
 
 urlpatterns = [
+    path('', views.inbox, name='inbox'),
+    path('<int:pk>', views.detail, name='detail'),
     path('new/<int:item_pk>', views.new_conversation, name='new'),
 ]
--- market2/converstation/views.py
+++ market2/converstation/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from items.models import Item
 from .models import *
+
+from .forms import *
 # Create your views here.
 
 def new_conversation(request, item_pk):
@@ -9,10 +12,11 @@ def new_conversation(request, item_pk):
     if item.created_by == request.user:#if you are the owener you should not be able to visit this page
         return redirect('dashboard:index')
 
-    conversation = conversation.objects.filter(item=item).filter(member__in=request.user.id)
+    conversation = Conversation.objects.filter(item=item).filter(member__in=[request.user.id]) #this shows existing conversation
 
     if conversation:
-        pass #redirect to conversation
+        # pass #redirect to conversation
+        return redirect('converstation:detail', pk=conversation.first().id)
 
     if request.method == 'POST':
         form = ConversationMessage(request.POST)
@@ -34,3 +38,36 @@ def new_conversation(request, item_pk):
     }
     
     return render(request, 'coversation/new.html', context)
+
+@login_required
+def inbox(request):
+    conversation = Conversation.objects.filter(member__in=[request.user.id])
+
+    context  = {
+        'conversation':conversation
+    }
+
+    return render(request, 'converstation/inbox.html', context)
+
+@login_required
+def detail(request, pk):
+    conversation = Conversation.objects.filter(member__in=[request.user.id]).get(pk=pk)
+    form = ConversationMessaageForm()
+
+    if request.method == 'POST':
+        form = ConversationMessaageForm(request.POST)
+        if form.is_valid():
+            conversation_message = form.save(commit=False)
+            conversation_message.conversation = conversation
+            conversation_message.created_by = request.user
+            conversation_message.save()
+
+            conversation.save()
+
+            return redirect('converstation:detail', pk=pk)
+
+    context={
+        'conversation':conversation,
+        'form':form
+    }
+    return render(request, 'converstation/detail.html', conversation)
