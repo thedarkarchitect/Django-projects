411e1fee0826aba6180f5783f58f45ada053672c
thedarkarchitect <romanreynolds240@gmail.com>
2023-02-12 13:11:47 +0300

    third commit

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Customer(models.Model):
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False, null=True, blank=True)#this will let us know if the product is a digital product or a physical product that needs to be shipped
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+    #this help us to access images as attributes using the model method below to stop a front end error we will use a try/except block
+    # the @property decorator will access the method like an attribut
+    # @property
+    # def imageURL(self):
+    #     try:
+    #         url = self.image.url
+    #     except:
+    #         url = ''
+    #     return url
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)#this is a one to one relationship
+    date_ordered = models.DateField(auto_now_add=True)
+    complete = models.BooleanField(default=False)#will keep track of cart items if they have been bought or not
+    transcation_id = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return str(self.id)
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)#this is a one to one relationship since and item need to align with  a product and order status
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)#this is a one to one relationship
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=200, null=False)
+    city = models.CharField(max_length=200, null=False)
+    state = models.CharField(max_length=200, null=False)
+    zipcode = models.CharField(max_length=200, null=False)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.address
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def store(request):
-    context = {}
+    products = Product.objects.all()
+
+    context = {
+        'products':products
+    }
     return render(request, 'store/store.html', context) 
 
 def cart(request):
-    context = {}
+
+    if request.user.is_authenticated:#will check if the customer is signed in and authenticated to accesss the cart of the specific customer
+        customer = request.user.customer#accessing the customer thru the user and customer one to one relationship
+        #this will allow to create an order or set an order
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()#this helps query child objects from parent in small caps from order class to all the items
+    else:
+        items = []
+
+    context = {'items':items}
     return render(request, 'store/cart.html', context) 
 
 def checkout(request):
