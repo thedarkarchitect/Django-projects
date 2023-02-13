d4860f2a9a9e0c3ae8c7b2a473032d0bdfc96e96
thedarkarchitect <romanreynolds240@gmail.com>
2023-02-13 10:02:29 +0300

    fifth commit

--- ecommerce/static/js/cart.js
+++ ecommerce/static/js/cart.js
@@ -12,8 +12,33 @@ for (i = 0; i < updateBtns.length; i++){
         if (user == 'AnonymousUser'){
             console.log('User is not authenticated')
         }else{
-            console.log('User is authenticated, sending data...')
+            updateUserOrder(productId, action)
         }
 
     })
+}
+
+function updateUserOrder(productId, action){
+    console.log('user is logged in, sending data..')
+
+    var url = '/update_item/'
+
+    fetch(url,{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json',
+            'X-CSRFToken' : csrftoken,
+        },
+        body:JSON.stringify({'productId': productId, 'action': action})
+    })
+
+    //response after sending data to the view
+    .then((response) => {
+        return response.json()
+    })
+
+    .then((data) => {
+        location.reload()
+        // console.log('data:', data)
+    });
 }
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
 from .models import *
 from django.http import JsonResponse
+import json
 
 # Create your views here.
 def store(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        items = []
+        order = {'get_Cart_total': 0, 'get_cart_items': 0}
+        cartItems = order['get_cart_items']
+
     products = Product.objects.all()
 
     context = {
-        'products':products
+        'products' : products,
+        'cartItems' : cartItems
     }
     return render(request, 'store/store.html', context) 
 
@@ -18,13 +30,17 @@ def cart(request):
         #this will allow to create an order or set an order
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()#this helps query child objects from parent in small caps from order class to all the items
+        cartItems = order.get_cart_items
+
     else:
         items = []
-        order = {'get_cart_total' : 0, 'get_Cart__items': 0 }
+        order = {'get_cart_total' : 0, 'get_Cart_items': 0 }
+        cartItems = order['get_Cart_items']
 
     context = {
         'items':items,
-        'order':order
+        'order':order,
+        'cartItems': cartItems
         }
     return render(request, 'store/cart.html', context) 
 
@@ -33,18 +49,41 @@ def checkout(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         #create empty cart for now for none-logged in users
         order = { 'get_cart_total':0, 'get_cart_items':0}
         items = []
+        cartItems = order['get_cart_items']
 
     context = {
         'items':items,
-        'order':order
+        'order':order,
+        'cartItems' : cartItems
         }
     return render(request, 'store/checkout.html', context) 
 
 def updateItem(request):
-    return JsonResponse(
-        'item was added', safe=False
-    )
+	data = json.loads(request.body)
+	productId = data['productId']
+	action = data['action']
+	print('Action:', action)
+	print('Product:', productId)
+
+	customer = request.user.customer
+	product = Product.objects.get(id=productId)
+	order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+	orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+	if action == 'add':
+		orderItem.quantity = (orderItem.quantity + 1)
+	elif action == 'remove':
+		orderItem.quantity = (orderItem.quantity - 1)
+
+	orderItem.save()
+
+	if orderItem.quantity <= 0:
+		orderItem.delete()
+
+	return JsonResponse('Item was added', safe=False)
