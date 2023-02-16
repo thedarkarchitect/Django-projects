16a498895e61f04c9f3992e8babd7ccf46e4ed91
thedarkarchitect <romanreynolds240@gmail.com>
2023-02-16 06:14:26 +0300

    third commit

--- studentManagementSystem/student/urls.py
+++ studentManagementSystem/student/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:id>', views.view_student, name='view_student'),
+    
 ]
--- studentManagementSystem/student/views.py
+++ studentManagementSystem/student/views.py
@@ -1,5 +1,8 @@
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render
 from .models import *
+
 
 # Create your views here.
 def index(request):
@@ -9,3 +12,7 @@ def index(request):
         'students' : students
     }
     return render(request, 'student/index.html', context)
+
+def view_student(request, id):
+    student = Student.objects.get(pk=id)#this means get me a student whose primary key in the db is passed as the id of the student clicked
+    return HttpResponseRedirect(reverse('index'))
