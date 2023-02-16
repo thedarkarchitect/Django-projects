1adfb997b6cf66e6fade2451ba7d1e2a30d0d8b9
thedarkarchitect <romanreynolds240@gmail.com>
2023-02-16 05:30:21 +0300

    second commit

--- studentManagementSystem/student/models.py
+++ studentManagementSystem/student/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    student_number = models.PositiveIntegerField()
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    field_of_study = models.CharField(max_length=200)
+    gpa = models.FloatField()
+
+    def __str__(self):
+        return f"Student: {self.first_name} {self.last_name}"
--- studentManagementSystem/student/views.py
+++ studentManagementSystem/student/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def index(request):
-    return render(request, 'student/index.html')
+    students = Student.objects.all()
+
+    context = {
+        'students' : students
+    }
+    return render(request, 'student/index.html', context)
