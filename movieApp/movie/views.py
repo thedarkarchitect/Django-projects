3d7c182722ef1c8646c85f7b2105bde6134f1550
thedarkarchitect <romanreynolds240@gmail.com>
2023-02-20 13:55:10 +0300

    ...

--- movieApp/movie/views.py
+++ movieApp/movie/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Movie
 from .forms import MovieForm
 
@@ -19,10 +19,25 @@ def info(request, id):
     return render(request, 'movie/info.html', context)
 
 def update(request, pk):
+    #this prepopulates the form
     movie = Movie.objects.get(pk=pk)
     form = MovieForm(instance=movie)
+
+    if request.method == 'POST':
+        movie = Movie.objects.get(pk=pk)
+        form = MovieForm( instance=movie)
+        if form.is_valid():
+            form.save()
+        return redirect('index')
+    
     context = {
         'movie':movie,
         'form':form
     }
     return render(request, 'movie/update.html', context)
+
+def delete(request, id):
+    if request.method == 'POST':
+        movie = Movie.objects.get(pk=id)
+        movie.delete()
+        return redirect('index')
--- netflix_app/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import Home
-
-app_name='core'#namespace
-
-urlpatterns = [
-    path('', Home.as_view())
-]
