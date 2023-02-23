59b1a505871ff6489a560d40dd116b4ae8604d9d
thedarkarchitect <romanreynolds240@gmail.com>
2023-02-23 07:32:52 +0300

    second commit

--- chatApp/chatApp/settings.py
+++ chatApp/chatApp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels',
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -68,6 +71,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'chatApp.wsgi.application'
+ASGI_APPLICATION = 'djangochat.asgi.application'#this for the chat connections
 
 
 # Database
---
+++ chatApp/core/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
+from django.contrib.auth import login
+
+
+# Create your views here.
+def frontpage(request):
+    return render(request, 'core/frontpage.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()#you put the current user into a variable to handle them specifically if need be
+            login(request, user)#this authenticated user
+
+            return redirect('frontpage')
+    else:
+        form = SignUpForm
+
+    context = {
+        'form' : form
+    }
+
+    return render(request, 'core/signup.html', context)
