0fe3305bba51b693a60b682b1a4d543184b5ff2d
thedarkarchitect <romanreynolds240@gmail.com>
2023-02-23 10:52:24 +0300

    final

--- chatApp/room/models.py
+++ chatApp/room/models.py
@@ -11,7 +11,7 @@ class Room(models.Model):
 
 class Message(models.Model):
     room = models.ForeignKey(Room, related_name='messages', on_delete=models.CASCADE)
-    user = models.ForeignKey(user, related_name='messages', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='messages', on_delete=models.CASCADE)
     content = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
 
